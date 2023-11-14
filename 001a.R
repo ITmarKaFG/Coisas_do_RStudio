316bd40a19d428846755874cecdf1dad0b01ccb2
FG <126037458+ITmarKaFG@users.noreply.github.com>
2023-11-14 13:14:11 +0000

    Add files via upload

---
+++ 001a.R
@@ -0,0 +1,7 @@
+vendas <- c(12,15,3,5) #este sistema são para vectores pequenos
+meses <- c('Mai','Jun','Jul','Agt') # c quer dizer variavel categorica
+# aqui a variável é chamada de objeto
+vendas2 <- scan() # outra forma de captar vetores atravez da consola
+
+Género <- c('Masculino','Feminino') 
+x <- 5
